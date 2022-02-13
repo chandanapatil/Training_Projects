7a580ef58076f4aa3c6f7791a782b6a008db86ac
chandanapatil <44522251+chandanapatil@users.noreply.github.com>
2022-02-13 20:08:02 +0530

    Add files via upload

---
+++ SecureApp/src/main/java/com/security/AppSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Bean public BCryptPasswordEncoder passwordEncoder() { return new
+			  BCryptPasswordEncoder(); }
+		
+	@Bean
+	public AuthenticationProvider authProvider() {
+		
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return provider;
+	}
+}
