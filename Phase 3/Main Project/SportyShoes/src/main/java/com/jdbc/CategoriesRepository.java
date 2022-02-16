6046258859c85c8f2c015427c26803269944c506
chandanapatil <44522251+chandanapatil@users.noreply.github.com>
2022-02-16 12:07:15 +0530

    Delete Phase 3/Main Project/SportyShoes directory

--- Phase 3/Main Project/SportyShoes/src/main/java/com/jdbc/CategoriesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jdbc;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.Categories;
-public interface CategoriesRepository extends JpaRepository<Categories, Long>{
-
-}
--- Phase 3/Main Project/SportyShoes/src/main/java/com/jdbc/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jdbc;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
